654ba07ea09987f574f9feb9d978b708564441d6
Philippe Gauthier <philippe@philippe.io>
2015-01-23 11:42:57 -0800

    refinements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,15 @@
 
 // Video Code
 
-var vid = document.getElementById("bgvid");
+if ($('#bgvid').length) {
+	var vid = document.getElementById("bgvid");
 var pauseButton = document.getElementById("vidpause");
 var controls = document.getElementById("controls");
 function vidFade() {
 vid.classList.add("stopfade");
 }
 vid.addEventListener('ended', function() {
-// only functional if "loop" is removed
 vid.pause();
-// to capture IE10
 vidFade();
 });
 pauseButton.addEventListener("click", function() {
@@ -18,20 +17,27 @@ vid.classList.toggle("stopfade");
 controls.classList.toggle("hide");
 pauseButton.classList.toggle("mask");
 if (vid.paused) {
-  
 vid.play();
-// pauseButton.innerHTML = "Pause";
 } else {
 vid.pause();
-  
-// pauseButton.innerHTML = "Paused";
 }
 });
+}
 
 
 
+$(document).ready(function() {
+  $('body').addClass('js');
+  var $menu = $('#menu'),
+    $menulink = $('.menu-link');
+  
+$menulink.click(function() {
+  $menulink.toggleClass('active');
+  $menu.toggleClass('active');
+  return false;
+});});
 
-// Google Analytics Code
+Google Analytics Code
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
